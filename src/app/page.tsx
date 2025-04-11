3fd86dc3e8845684295a2ba82c749911f50e95f6
krytigx <kryptigx@gmail.com>
2025-04-11 21:21:52 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
           {/* Row 1: Name and Image */}
           <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6">
             <motion.h1
-              className="leading-none text-[8rem] sm:text-[6rem] md:text-[12rem]"
+              className="mb-4 leading-none text-[7rem] md:text-[12rem]" 
             >
               <motion.span
                 initial={{ y: 200, opacity: 0 }}
@@ -49,6 +49,7 @@ export default function HomePage() {
                 GARG
               </motion.span>
             </motion.h1>
+
 
 
             <motion.div
