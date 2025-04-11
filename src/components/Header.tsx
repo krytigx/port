53e0b8f4e7875bc8153c29cb5a81798bf0f9030e
krytigx <kryptigx@gmail.com>
2025-04-11 22:49:34 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,10 @@ import { useState } from "react";
 
 const navItems = [
   { name: "Works", path: "/works" },
+  { name: "Resume", path: "https://docs.google.com/document/d/1ZRnGm8sv9rWIjJpl9enTNh8Rkty7aDSwK1j8OkAlCUU/edit?usp=sharing"}
   { name: "About", path: "/about" },
   { name: "Contact", path: "/contact" },
+  
 ];
 
 export function Header() {
