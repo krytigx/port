f066659077f9730f9c2ab29343afd588cfd4e51c
krytigx <kryptigx@gmail.com>
2025-04-11 22:14:52 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,12 +53,11 @@ export default function HomePage() {
 
 
             <motion.div
-              className="w-full md:w-1/3 flex flex-col items-center md:items-end"
+              className="w-[15rem] h-[15rem] relative overflow-hidden rounded-full"
               initial={{ scale: 0.8, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 0.8, delay: 0.4 }}
+              transition={{ duration: 0.8, delay: 1 }}
             >
-
               <Image
                 src="https://raw.githubusercontent.com/krytigx/Portfolio_images/refs/heads/main/place_holder.jpg"
                 alt="kryptigx"
